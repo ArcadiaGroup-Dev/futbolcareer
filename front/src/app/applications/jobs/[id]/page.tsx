8896f262043b3b659b6cb6995f9f3eb9255a914f
Martin Eduardo Aguayo <martineduardoaguayo@gmail.com>
2025-05-15 22:02:08 -0300

    update workflow front

--- front/src/app/applications/jobs/[id]/page.tsx
+++ front/src/app/applications/jobs/[id]/page.tsx
@@ -8,6 +8,85 @@ const JobApplicationsPage: React.FC = () => {
   const { id } = useParams(); // Accedemos al parámetro `id` directamente desde la URL
   const [jobId, setJobId] = useState<string | null>(null);
 
+  const asd = [
+    {
+        "id": "ffe9bae3-17a4-4a1e-a7e4-6caee4b30472",
+        "message": "Mensaje de aplicación",
+        "status": "PENDING",
+        "appliedAt": "2025-05-08T20:59:43.116Z",
+        "player": {
+            "id": "16a3ee5c-64a7-428c-a000-b84ae3112763",
+            "name": "Mel",
+            "ubicacionActual": "Argentina",
+            "lastname": "Hell",
+            "nameAgency": null,
+            "email": "test@gmail.com",
+            "password": "$2b$10$WQQPk8r9iT/1E/Sg/YM3tu4cX97sEmaAgMoeq2E7mdpBoynhCARpW",
+            "puesto": "Jugador",
+            "role": "PLAYER",
+            "imgUrl": null,
+            "phone": null,
+            "nationality": "",
+            "location": null,
+            "genre": "Femenino",
+            "birthday": null,
+            "countryToWork": null,
+            "pasaporteUe": null,
+            "height": null,
+            "age": null,
+            "weight": null,
+            "skillfulFoot": null,
+            "bodyStructure": null,
+            "habilities": null,
+            "primaryPosition": null,
+            "secondaryPosition": null,
+            "videoUrl": null,
+            "trayectorias": null,
+            "socialMedia": null,
+            "subscription": null,
+            "cv": null
+        }
+    },
+    {
+        "id": "233b8baa-c56f-43f1-aba1-7db4980a7e3a",
+        "message": "Mensaje de aplicación",
+        "status": "PENDING",
+        "appliedAt": "2025-05-10T01:11:56.642Z",
+        "player": {
+            "id": "6e19ca43-9770-4d1c-ae1c-61d7e1d149c4",
+            "name": "federico",
+            "ubicacionActual": "buenos aires ",
+            "lastname": "suarez",
+            "nameAgency": null,
+            "email": "coflipweb@gmail.com",
+            "password": "$2b$10$RyZ7y9sC49WjRH0GvzUpke5gKf6tR.JKWFBMV0xsPgKOCEESBN9N2",
+            "puesto": "Entrenador",
+            "role": "PLAYER",
+            "imgUrl": null,
+            "phone": null,
+            "nationality": "",
+            "location": null,
+            "genre": "Masculino",
+            "birthday": null,
+            "countryToWork": null,
+            "pasaporteUe": null,
+            "height": null,
+            "age": null,
+            "weight": null,
+            "skillfulFoot": null,
+            "bodyStructure": null,
+            "habilities": null,
+            "primaryPosition": null,
+            "secondaryPosition": null,
+            "videoUrl": null,
+            "trayectorias": null,
+            "socialMedia": null,
+            "subscription": null,
+            "cv": null
+        }
+    }
+]
+
   useEffect(() => {
     // Aseguramos que `id` sea un string antes de asignarlo a `jobId`
     if (typeof id === "string") {
